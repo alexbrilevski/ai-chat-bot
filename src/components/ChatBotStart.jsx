df879c01df33901683ea54fdcfeb06f50445c444
Alex Brilevski <brilevski.alex@gmail.com>
2026-09-17 17:21:11 +0300

    The start page markup and style were added

--- src/components/ChatBotStart.jsx
+++ src/components/ChatBotStart.jsx
@@ -1,5 +1,11 @@
+import './ChatBotStart.css';
+
 const ChatBotStart = () => {
-  return <div>ChatBotStart</div>;
+  return (
+    <div className="start-page">
+      <button className="start-page-btn">Chat AI</button>
+    </div>
+  );
 };
 
 export default ChatBotStart;
