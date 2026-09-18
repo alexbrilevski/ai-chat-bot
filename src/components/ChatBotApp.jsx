11062779678a240fff703c7e7f46a7c77352ea3c
Alex Brilevski <brilevski.alex@gmail.com>
2026-09-18 20:58:33 +0300

    The chat page markup and style were added

---
+++ src/components/ChatBotApp.jsx
@@ -0,0 +1,50 @@
+import "./ChatBotApp.css";
+
+const ChatBotApp = () => {
+  return (
+    <div className="chat-app">
+      <div className="chat-list">
+        <div className="chat-list-header">
+          <h2>Chat List</h2>
+          <i className="bx bx-edit-alt new-chat"></i>
+        </div>
+        <div className="chat-list-item active">
+          <h4>Chat 18/09/2026 12:35:45 PM</h4>
+          <i className="bx bx-x-circle"></i>
+        </div>
+        <div className="chat-list-item">
+          <h4>Chat 18/09/2026 12:35:45 PM</h4>
+          <i className="bx bx-x-circle"></i>
+        </div>
+      </div>
+      <div className="chat-window">
+        <div className="chat-title">
+          <h3>Chat with AI</h3>
+          <i className="bx bx-arrow-back arrow"></i>
+        </div>
+        <div className="chat">
+          <div className="promt">
+            Hi, how are you? <span>12:35:45 PM</span>
+          </div>
+          <div className="response">
+            Hello! I'm just a computer program, so I don't have feelings, but
+            I'm here and ready to assist you. How can I help you today?
+            <span>12:35:55 PM</span>
+          </div>
+          <div className="typing">Typing...</div>
+          <form className="msg-form">
+            <i className="fa-solid fa-face-smile emoji"></i>
+            <input
+              type="text"
+              className="msg-input"
+              placeholder="Type a message..."
+            />
+            <i className="fa-solid fa-paper-plane"></i>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatBotApp;
